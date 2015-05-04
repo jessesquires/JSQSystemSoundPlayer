d695f5b722b6bc43b57473ac950a7ccff1207384
Jesse Squires <jesse.d.squires@gmail.com>
2015-05-03 17:09:03 -0700

    refactor, reorganize. switch to framework. ref #12

---
+++ Example/Example/ViewController.m
@@ -0,0 +1,82 @@
+//
+//  Created by Jesse Squires
+//  http://www.jessesquires.com
+//
+//
+//  Documentation
+//  http://cocoadocs.org/docsets/JSQSystemSoundPlayer
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/JSQSystemSoundPlayer
+//
+//
+//  License
+//  Copyright (c) 2015 Jesse Squires
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+#import "ViewController.h"
+
+#import <JSQSystemSoundPlayer/JSQSystemSoundPlayer.h>
+
+
+@implementation ViewController
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.soundSwitch.on = [JSQSystemSoundPlayer sharedPlayer].on;
+}
+
+#pragma mark - Actions
+
+- (IBAction)playSystemSoundPressed:(UIButton *)sender
+{
+    JSQSystemSoundPlayer *sharedPlayer = [JSQSystemSoundPlayer sharedPlayer];
+    
+    [sharedPlayer playSoundWithFilename:@"Basso"
+                          fileExtension:kJSQSystemSoundTypeAIF
+                             completion:^{
+                                 NSLog(@"Sound finished playing. Executing completion block...");
+                             }];
+}
+
+- (IBAction)playAlertSoundPressed:(UIButton *)sender
+{
+    [[JSQSystemSoundPlayer sharedPlayer] playAlertSoundWithFilename:@"Funk"
+                                                      fileExtension:kJSQSystemSoundTypeAIFF];
+}
+
+- (IBAction)playVibratePressed:(UIButton *)sender
+{
+    [[JSQSystemSoundPlayer sharedPlayer] playVibrateSound];
+}
+
+- (IBAction)playLongSoundPressed:(UIButton *)sender
+{
+    NSLog(@"Playing long sound...");
+    [[JSQSystemSoundPlayer sharedPlayer] playSoundWithFilename:@"BalladPiano"
+                                                 fileExtension:kJSQSystemSoundTypeCAF
+                                                    completion:^{
+                                                        NSLog(@"Long sound complete!");
+                                                    }];
+}
+
+- (IBAction)stopPressed:(UIButton *)sender
+{
+    [[JSQSystemSoundPlayer sharedPlayer] stopAllSounds];
+    
+    // Stop playing specific sound
+    // [[JSQSystemSoundPlayer sharedPlayer] stopSoundWithFilename:@"BalladPiano"];
+}
+
+- (IBAction)toggleSwitch:(UISwitch *)sender
+{
+    [[JSQSystemSoundPlayer sharedPlayer] toggleSoundPlayerOn:sender.on];
+}
+
+@end
--- JSQSystemSoundPlayer.podspec
+++ JSQSystemSoundPlayer.podspec
@@ -9,7 +9,7 @@ Pod::Spec.new do |s|
    s.author = { 'Jesse Squires' => 'jesse.squires.developer@gmail.com' }
 
    s.source = { :git => 'https://github.com/jessesquires/JSQSystemSoundPlayer.git', :tag => s.version }
-   s.source_files = 'JSQSystemSoundPlayer/Classes/*'
+   s.source_files = 'JSQSystemSoundPlayer/JSQSystemSoundPlayer/*.{h,m}'
 
    s.ios.deployment_target = '6.0'
    s.osx.deployment_target = '10.7'
