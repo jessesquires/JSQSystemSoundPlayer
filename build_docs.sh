5f4db60553d7ae889b93c4839d2b639cdadae290
Jesse Squires <jesse.d.squires@gmail.com>
2015-05-03 18:57:30 -0700

    add build docs script

---
+++ build_docs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+appledoc --create-html --no-install-docset --docset-install-path ./_docs --project-name JSQSystemSoundPlayer --project-version 3.0.0 --project-company "Jesse Squires" --company-id com.jessesquires --output ./_docs JSQSystemSoundPlayer/JSQSystemSoundPlayer/
