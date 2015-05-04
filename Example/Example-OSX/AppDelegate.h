cadd1d39a6785cf79f4943db62244465c257d5e2
Jesse Squires <jesse.d.squires@gmail.com>
2015-05-03 17:32:49 -0700

    re-add OSX project

---
+++ Example/Example-OSX/AppDelegate.h
@@ -0,0 +1,39 @@
+//
+//  Created by Jesse Squires
+//  http://www.jessesquires.com
+//
+//
+//  Documentation
+//  http://cocoadocs.org/docsets/JSQSystemSoundPlayer
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/JSQSystemSoundPlayer
+//
+//
+//  License
+//  Copyright (c) 2015 Jesse Squires
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+@import Cocoa;
+
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (weak) IBOutlet NSWindow *window;
+
+@property (weak, nonatomic) IBOutlet NSButton *soundCheckbox;
+
+- (IBAction)playSystemSoundPressed:(NSButton *)sender;
+
+- (IBAction)playAlertSoundPressed:(NSButton *)sender;
+
+- (IBAction)playLongSoundPressed:(NSButton *)sender;
+
+- (IBAction)stopPressed:(NSButton *)sender;
+
+- (IBAction)toggleSwitch:(NSButton *)sender;
+
+@end
+
