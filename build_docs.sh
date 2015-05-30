c21aff8b5a442fc3b21d290cab2ec1b51a940d70
Jesse Squires <jesse.d.squires@gmail.com>
2015-05-30 12:13:25 -0700

    remove build docs script

--- build_docs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-appledoc --create-html --no-install-docset --docset-install-path ./_docs --project-name JSQSystemSoundPlayer --project-version 3.0.0 --project-company "Jesse Squires" --company-id com.jessesquires --output ./_docs JSQSystemSoundPlayer/JSQSystemSoundPlayer/
