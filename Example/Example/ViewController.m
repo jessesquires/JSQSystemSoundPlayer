707c8cd0f9950add93e1a1ee3e430bee2ce614dc
LastObject <lastobject@gmail.com>
2016-04-13 08:58:19 +0800

    play predefined system sound

--- Example/Example/ViewController.m
+++ Example/Example/ViewController.m
@@ -54,7 +54,7 @@
 
 - (IBAction)playVibratePressed:(UIButton *)sender
 {
-    [[JSQSystemSoundPlayer sharedPlayer] playSoundWithsoundID:1000 isAlert:NO completionBlock:nil];
+    [[JSQSystemSoundPlayer sharedPlayer] playVibrateSound];
 }
 
 - (IBAction)playLongSoundPressed:(UIButton *)sender
