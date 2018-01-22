d7f24a4637ba369a22027d817532a8f977ffa761
Jesse Squires <jessesquires@users.noreply.github.com>
2018-01-22 14:57:10 -0800

    Update build_docs.sh

--- build_docs.sh
+++ build_docs.sh
@@ -4,15 +4,23 @@
 # https://github.com/realm/jazzy
 # ------------------------------
 
-jazzy \
-    --clean \
-    --author 'Jesse Squires' \
-    --author_url 'https://twitter.com/jesse_squires' \
-    --github_url 'https://github.com/jessesquires/JSQSystemSoundPlayer' \
-    --module 'JSQSystemSoundPlayer' \
-    --umbrella-header 'Source/JSQSystemSoundPlayer.h' \
-    --framework-root . \
-    --objc \
-    --readme 'README.md' \
-    --documentation 'Guides/*.md' \
-    --output docs/ \
+if which jazzy >/dev/null; then
+    jazzy \
+        --clean \
+        --author 'Jesse Squires' \
+        --author_url 'https://twitter.com/jesse_squires' \
+        --github_url 'https://github.com/jessesquires/JSQSystemSoundPlayer' \
+        --module 'JSQSystemSoundPlayer' \
+        --umbrella-header 'Source/JSQSystemSoundPlayer.h' \
+        --framework-root . \
+        --objc \
+        --readme 'README.md' \
+        --documentation 'Guides/*.md' \
+        --output docs/ \;
+else
+    echo "
+    Error: jazzy not installed! <https://github.com/realm/jazzy>
+    Install: gem install jazzy
+    "
+    exit 1
+fi
